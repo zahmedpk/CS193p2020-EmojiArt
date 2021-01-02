e9762efa2146da3b4184e8199049b9234dabe5a5
Zubair Ahmed <zahmed.pk@gmail.com>
2021-01-02 07:31:28 +0500

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Zubair Ahmed on 02/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
